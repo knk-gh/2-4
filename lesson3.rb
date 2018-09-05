94c574a397cd6a80d93d4527cf1b992cb89de5cc
knk <knk@knknoMacBook-puro.local>
2018-09-05 15:22:54 +0900

    [Finish]2-4

---
+++ lesson3.rb
@@ -0,0 +1,17 @@
+puts "webcamp"
+
+puts "webcamp".upcase
+
+puts "WEBCAMP"
+
+puts "WEBCAMP".downcase
+
+puts "WeBcAmP".swapcase
+
+puts "webcamp".capitalize
+
+puts "今２０１８年です"
+
+puts "今２０１８年です".tr("０-９ａ-ｚＡ-Ｚ","0-9a-zA-Z")
+
+puts "あいうえおかきくけこ".tr("あ-お","ア-オ")
